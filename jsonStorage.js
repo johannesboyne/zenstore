a9ed7a702d5709c383e755053a4634358669e4f7
Johannes Boyne <johannes@boyne.de>
2014-02-07 12:21:44 +0100

    v0.0.2

--- jsonStorage.js
+++ jsonStorage.js
@@ -1,16 +1,17 @@
 var levelup = require('levelup');
 var storage = levelup('./zenstorage');
-module.exports.store = function (options) {
+module.exports.store = function (options, fn) {
     if (!options.id) {
         return null;
     }
-    storage.put(options.id, options.data);
+    storage.put(options.id, options.data, fn);
 };
 
 module.exports.get = function (id, fn) {
     storage.get(id, function (err, value) {
         if (err) {
-            if (err.notFound)  return fn('["othing found"]'); // nothing found
+            if (err.notFound)  return fn('["nothing found"]'); // nothing found
+            else return fn('["system error"]');
         }
         return fn(value);
     });
