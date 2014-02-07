ee119fe4059347e7f048486dd45f73d1055ee51d
Johannes Boyne <johannes@boyne.de>
2014-02-07 08:33:22 +0100

    v.0.0.0

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 var linkCreator = require('./linkCreator'),
-    jsonStorage = require('./jsonStorage');
+    jsonStorage = require('./jsonStorage'),
+    jade = require('jade');
 
 require('http').createServer(function (req, res) {
-    if (req.url.length === 41) {
-        res.writeHead(200, {'content-type': 'application/json'});
-        res.end(jsonStorage.get(req.url.replace(/\//, '')));
+    if (req.method === 'POST' && req.url.length === 41) {
+        var data = '';
+        req.on('data', function (chunk) {
+            data += chunk.toString();
+        });
+        req.on('end', function () {
+            if (data.length > 0) {
+                jsonStorage.store({id: req.url.replace(/\//, ''), data: data});
+                res.writeHead(200, {'content-type': 'application/json'});
+                res.end(data);
+            }
+        });
     } else {
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.end('your private zenstorage URL is: ' + req.headers.host + '/' + linkCreator.createLink());
+        if (req.url.length === 41) {
+            res.writeHead(200, {'content-type': 'application/json'});
+            jsonStorage.get(req.url.replace(/\//, ''), function (value) {
+                res.end(value);
+            });
+        } else {
+            res.writeHead(200, {'content-type': 'text/html'});
+            res.end(jade.renderFile('./zenstore.jade', {zenlink: req.headers.host + '/' + linkCreator.createLink()}));
+        }
     }
 }).listen(process.env.PORT || 1337);
--- jsonStorage.js
+++ jsonStorage.js
@@ -1,11 +1,17 @@
-var localStorage = {};
+var levelup = require('levelup');
+var storage = levelup('./zenstorage');
 module.exports.store = function (options) {
     if (!options.id) {
         return null;
     }
-    localStorage[options.id] = options.data;
+    storage.put(options.id, options.data);
 };
 
-module.exports.get = function (id) {
-    return String(localStorage[id]);
+module.exports.get = function (id, fn) {
+    storage.get(id, function (err, value) {
+        if (err) {
+            if (err.notFound)  return fn('["othing found"]'); // nothing found
+        }
+        return fn(value);
+    });
 };
